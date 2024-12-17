27eebe386f9ed2b37783ec09fea255deef417a7e
= <=>
2024-12-17 19:46:20 +0545

    feat : learning about the concept of list, nested_list,how to add by doing append,input syntax to take input from the user and store it into the empty list, sorting numbers by the help of sort method,knowing the concept of index inside the list as well as normal ...and knowing basics too...

--- day_1/day_1_learn.py
+++ day_1/day_1_learn.py
@@ -1,1 +1,35 @@
 print("hello world")
+
+
+a ="apple"
+b = ['apple',2,2.4,"rahul"]
+print(b)
+print(b[2])
+
+matrix_1=[1, 2,3,7,9,0,5]
+matrix_1.sort()
+
+print(matrix_1)
+matrix_1.pop()
+
+print(matrix_1)
+
+#append
+d = matrix_1.append([4])
+
+nested_list =[[1,2,3,4,5], [5,6,7,0]]
+
+print(f"This is the nested list's item\:{nested_list[0][2]}")
+
+store= 2*matrix_1
+print(store)
+
+number = input(" enter any number: ")
+
+empty_list = []
+
+empty_list.append(number)
+print(empty_list)
+
+j=["anil","kapoor",23]
+print(j)
