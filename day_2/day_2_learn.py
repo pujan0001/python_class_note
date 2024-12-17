7e6c3c467fd9615c0b8748585f6a71717a69ac74
= <=>
2024-12-17 19:53:30 +0545

    feat : fixing the bug that give none as a output while running it into the variable that is assigned...

---
+++ day_2/day_2_learn.py
@@ -0,0 +1,37 @@
+
+test_matrix = [1,2,3,4,5,6]
+
+test_matrix_1 =[7,8,9]
+test_matrix.extend(test_matrix_1)
+print(test_matrix)
+i= input("Enter the number:")
+i = int(i) #typecasting
+#print(type(i))
+
+k=4
+o="apple"
+next_matrix=[k,o]
+print(next_matrix[0])
+
+print(len(next_matrix))
+
+another_matrix=[len(next_matrix),4,"apple"]
+print(another_matrix)
+
+l= ["apple","goat", "cow", "goat"]
+l.remove("goat")
+print(l)
+
+m=[1,3,4,5,6,"football"]
+print(m[-1])
+print(m[::-1]) #-1 dekhi SABBAI
+
+test_variables=[1,6,8,9,3,0]
+test_variables.sort()
+print(test_variables)
+#print(test_variables)
+#print("Following is descending order")
+#print(test_variables[::-1])
+
+#if 5>2:
+#    print("Five is greater than two")
