729e9f4bd385272aca7ddf48a923e44189226ef8
= <=>
2024-12-17 21:20:20 +0545

    feat: commit done for ...

---
+++ day_4/day_4_learn.py
@@ -0,0 +1,54 @@
+#test_2= ["apple","banana","cat","dog"]
+#for items,index in enumerate(test_2):
+  #  print(index,items)
+
+#test_2 = ["apple", "banana", "cat", "dog"]
+
+#index = 0 
+#for item in test_2:
+   # print(item,index )
+  #  index += 1 
+# test_2 = ["apple", "banana", "cat", "dog"]
+# for items in test_2:
+#     print(f"The item is {items} and it's index is {test_2.index(items)}")
+
+  #  test_4=20
+ #   if test_4>10:
+#        if test_4<30:
+        
+# a= input ("Enter the number:")
+# a=int(a)
+
+# if a>10:
+#     print(f"Given value is greater than 10 yayyy..")
+
+# elif a<=100:
+#     print(f"Given value is less than or equals to 100")
+
+# else:
+#     print(f"This was the last condition check")
+
+# for i in range(30,60):
+#     print(i)
+
+# a= input("Enter the number:")
+# a= int(a)
+
+# for a in range(30,60):
+#  print(a)
+
+
+# a={"name":"bishal","last_name":"kharal","role":"mentor"}
+# user_keys= list(a.keys())
+# print(user_keys)
+
+# for i in user_keys:
+#    print(i)
+
+# test_1=[]
+lst=[]
+for i in range(10):
+   a=int(input("enter a number:\n"))
+   
+   lst.append(a)
+   print(lst)
